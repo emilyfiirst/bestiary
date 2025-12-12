a2beb7e6b691f8a21ddcbef06e5207444729525d
emilyfiirst <emiliany@live.com>
2025-12-11 21:32:31 -0300

    fiirst commit

---
+++ src/main/java/io/emilyfiirst/bestiary/BestiaryApplication.java
@@ -0,0 +1,13 @@
+package io.emilyfiirst.bestiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestiaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/emilyfiirst/bestiary/BestiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package io.emilyfiirst.bestiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
